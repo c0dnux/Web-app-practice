22eb0b8497a58ca82644c40cecf0b3c9c7bb6814
Abdulrahman sani <sanuabdulrahaman851@gmail.com>
2024-12-17 10:51:29 +0100

    stats-commit

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require("./../controllers/tourcontroller");
 
 const router = express.Router();
 router.route("/stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
