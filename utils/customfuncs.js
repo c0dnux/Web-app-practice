0802b7c6e72586ce6e3c3b2deeb7af4410a7046f
Abdulrahman sani <sanuabdulrahaman851@gmail.com>
2025-01-13 16:32:13 +0100

    rate-limiting-middleware

--- utils/customfuncs.js
+++ utils/customfuncs.js
@@ -2,20 +2,23 @@ const jwt = require("jsonwebtoken");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_COOKIE_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
-exports.signTokenHandler = (statusCode, message, res, user) => {
+exports.signTokenHandler = (statusCode, message, res, user, trialLimit) => {
+  console.log(trialLimit);
+
   const token = signToken(user._id);
+
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Ensure secure in production
+    secure: process.env.NODE_ENV === "production",
   };
-  res.cookie("jwt", cookieOptions, token);
-  user.passWord = "**********";
+  res.cookie("jwt", token, cookieOptions);
+  user.passWord = undefined;
   res.status(statusCode).json({
     status: "Success",
     token,
