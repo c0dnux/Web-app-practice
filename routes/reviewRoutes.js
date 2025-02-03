7d62eda87180944bc65a57d01f04a4097400a96a
Abdulrahman sani <sanuabdulrahaman851@gmail.com>
2025-02-03 10:58:45 +0100

    review-done

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const reviewsController = require("../controllers/reviewsController");
 const authController = require("../controllers/authcontroller");
+//Post /tour/234fad4/reviews  //Create a review
+//Get /tour/234fad4/reviews  //Get all reviews
+//Get /tour/234fad4/reviews/234fad4   //Get a specific review
+// router
+//   .route("/:tourId/reviews")
+//   .post(authController.protect, reviewsController.makeReview)
+//   .get(authController.protect, reviewsController.getAllReviews);
+// router
+//   .route("/:tourId/reviews/:reviewId")
+//   .get(authController.protect, reviewsController.getReview);
+
 router
   .route("/")
   .post(authController.protect, reviewsController.makeReview)
-  .get(authController.protect, reviewsController.getAllReviews);
+  .get(authController.protect, reviewsController.getAllReviews)
+  .patch(authController.protect, reviewsController.updateReview);
+
+router
+  .route("/:id")
+  .get(authController.protect, reviewsController.getReview)
+  .delete(authController.protect, reviewsController.deleteReview);
 module.exports = router;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const tourController = require("./../controllers/tourcontroller");
 const authController = require("./../controllers/authcontroller");
-const reviewsController = require("../controllers/reviewsController");
-
+const reviewRoutes = require("./reviewRoutes");
 const router = express.Router();
+
+//Post /tour/234fad4/reviews  //Create a review
+//Get /tour/234fad4/reviews  //Get all reviews
+//Get /tour/234fad4/reviews/234fad4   //Get a specific review
+// router
+//   .route("/:tourId/reviews")
+//   .post(authController.protect, reviewsController.makeReview)
+//   .get(authController.protect, reviewsController.getAllReviews);
+// router
+//   .route("/:tourId/reviews/:reviewId")
+//   .get(authController.protect, reviewsController.getReview);
+router.use("/:tourId/reviews", reviewRoutes);
+
 router.route("/stats").get(tourController.getTourStats);
 router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 
@@ -23,14 +35,5 @@ router
     authController.restrictTo("admin", "lead-guide"),
     tourController.deletTour
   );
-//Post /tour/234fad4/reviews  //Create a review
-//Get /tour/234fad4/reviews  //Get all reviews
-//Get /tour/234fad4/reviews/234fad4   //Get a specific review
-router
-  .route("/:tourId/reviews")
-  .post(authController.protect, reviewsController.makeReview)
-  .get(authController.protect, reviewsController.getAllReviews);
-router
-  .route("/:tourId/reviews/:reviewId")
-  .get(authController.protect, reviewsController.getReview);
+
 module.exports = router;
